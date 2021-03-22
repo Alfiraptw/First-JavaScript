d3cc0dd094a8c94b31449c01025204d8061552d7
Alfiraptw <80810295+Alfiraptw@users.noreply.github.com>
2021-03-22 13:44:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// const judul = document.getElementById('judul');
+// judul.style.color = 'red';
+// judul.style.backgroundColor = '#cc';
+// judul.innerHTML = 'BelajarDOM';
+
+
+const p = document.getElementsByTagName('p');
+p[0].style.backgroundColor = 'lightblue';
+p[1].style.backgroundColor = 'lightblue';
+p[2].style.backgroundColor = 'yellow';
+p[3].style.backgroundColor = 'green';
+
+
+// for( let i =0; i < p.length; i++){
+//     p[1].style.backgroundColor = 'lightblue';
+// }
+
+
+// const p1 = document.getElementsByClassName('p1');
+// p1[0].innerHTML = 'ini diubah dari password'
+
+
+// const p4 = document.querySelector('#b p');
+// p4.style.color = 'blue';
+// // p4.style.fontSize = '30px';
+
+const li2 = document.querySelector('section#b ul li:nth-child(2)');
+li2.style.backgroundColor = 'orange';
+
+// const p = document.querySelector('p');
+// p.innerHTML = 'ini diubah melalui javascript';
+
+
+// const p = document.querySelectorAll('p');
+
+// p[2].style.backgroundColor = 'lightblue';
